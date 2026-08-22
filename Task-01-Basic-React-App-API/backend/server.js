f328bf4759c30b2f00870b2fc86908deb3a1a76a
Walmik Pravin Ingle <inglewalmik68@gmail.com>
2026-08-23 00:12:22 +0530

    Mongo Atlas Configurations

--- Task-01-Basic-React-App-API/backend/server.js
+++ Task-01-Basic-React-App-API/backend/server.js
@@ -1,7 +1,9 @@
-const express = require("express")
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const products = [
   {
@@ -32,6 +34,17 @@ app.get("/api/products", (req,res)=> {
   res.json(products);
 });
 
-app.listen(PORT,() => {
+
+
+mongoose 
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("MongoDB Connected Successfully");
+    app.listen(PORT,() => {
   console.log(`Server running on https://localhost:${PORT}`);
 });
+  })
+
+  .catch((error) => {
+    console.log("Mongo DB COnnecion Failed: ", error.message);
+  })
